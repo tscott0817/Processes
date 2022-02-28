4bc268a7febefdcc1c056d1c19b4ef3ea8d32bba
Tyler <tylerscott.home@gmail.com>
2022-02-28 09:45:14 -0500

    First

--- processes.tscott5.c
+++ processes.tscott5.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <signal.h>
@@ -7,11 +8,19 @@
 
 #define BUFFER_SIZE 32
 
-int done;
+int doneParent;
+int doneChild;
 
-void handler1(int signum) {
-  printf("this is handler1(): got a signal %d\n", signum);
-  done = 1;
+void handlerParent(int signum) {
+  printf("----------------------------------------------\n");
+  printf("Got signal %d\n", signum);
+  doneParent = 1;
+}
+
+void handlerChild(int signum) {
+  printf("----------------------------------------------\n");
+  printf("Got signal %d\n", signum);
+  doneParent = 1;
 }
 
 int main(int argc, char *argv[]) {
@@ -21,8 +30,10 @@ int main(int argc, char *argv[]) {
   int key = IPC_PRIVATE;
   char *ptr;
   char buffer[BUFFER_SIZE];
-  struct sigaction action;
-  done = 0;
+  struct sigaction actionParent;
+  struct sigaction actionChild;
+  doneParent = 0;
+  doneChild = 0;
 
   // shmget() returns the shared memory identifier associated with 'key'
   // key is of 'key_t' type; for requesting resources (like shared mem)
@@ -30,15 +41,19 @@ int main(int argc, char *argv[]) {
   memid = shmget(key, BUFFER_SIZE, IPC_EXCL | 0666);
   
   // Sets 'action' struct equal to void handler1 'object' (Int to be passed in later?)
-  memset(&action, 0, sizeof(struct sigaction));
-  action.sa_handler = handler1;
+  memset(&actionParent, 0, sizeof(struct sigaction));
+  actionParent.sa_handler = handlerParent;
+
+  memset(&actionChild, 0, sizeof(struct sigaction));
+  actionChild.sa_handler = handlerParent;
 
   // sigaction is a built in function. Sets program behavior when receiving certain OS signals. 
-  // SIGUSR1 is a signal, used for interprocess communication
+  // SIGUSR1 is a user defined signal, used for interprocess communication
   // &action is a reference to the newly set action struct
   // So I guess when -SIGUSR1 is calld in terminal, the action of the handler1() function will be executed,
-  // and set 'done' equal to 1 which allow while loop to end.
-  sigaction(SIGUSR1, &action, NULL);
+  // and set 'done' equal to 1 which allows while loop to end.
+  sigaction(SIGUSR1, &actionParent, NULL);
+  sigaction(SIGUSR2, &actionChild, NULL);
 
   if (memid < 0) {
     printf("shmget() failed\n");
@@ -58,8 +73,9 @@ int main(int argc, char *argv[]) {
   // fork() returns process id of child to parent 
   if (pid > 0) {
 
-    // this is the parent
     printf("I am the parent, and my pid is %d\n", getpid());
+    while(!doneParent);
+    doneParent = 0;
 
     // shmat() is 'shared memory attach'
     // memid is the id of the shared memory
@@ -71,73 +87,92 @@ int main(int argc, char *argv[]) {
       return(8);
     }
 
-    while(!done);
-
     strcpy(buffer, "hello");
-    printf("Parent is writing '%s' to the shared memory\n", buffer);
-    
+    printf("Parent is writing '%s' to the shared memory\n", buffer);   
+
     // Copy the contents of the buffer to ptr of the share mem id attach; shmat()
     strcpy(ptr, buffer);
-    //wait(NULL);
-    while(!done);
+    while(!doneParent);
+    doneParent = 0;
 
-    //pid = fork();
     strcpy(buffer, "from");
+    printf("Parent is writing '%s' to the shared memory\n", buffer);    
+
+    // Copy the contents of the buffer to ptr of the share mem id attach; shmat()
+    strcpy(ptr, buffer);
+    while(!doneParent);
+    doneParent = 0;
+
+    strcpy(buffer, "tyler");
+    printf("Parent is writing '%s' to the shared memory\n", buffer);    
+
+    // Copy the contents of the buffer to ptr of the share mem id attach; shmat()
+    strcpy(ptr, buffer);
+    while(!doneParent);
+    doneParent = 0;
+
+    strcpy(buffer, "done");
     printf("Parent is writing '%s' to the shared memory\n", buffer);
 
     // Copy the contents of the buffer to ptr of the share mem id attach; shmat()
     strcpy(ptr, buffer);
-    //wait(NULL);
-    while(!done);
+    while(!doneParent);
+    doneParent = 0;
 
-    strcpy(buffer, "Tyler");
-    printf("Parent is writing '%s' to the shared memory\n", buffer);
-
-    // Copy the contents of the buffer to ptr of the share mem id attach; shmat()
-    strcpy(ptr, buffer);
-    //wait(NULL);
-    while(!done);
   } 
 
-  // if pid == 0; means child
+  // if pid == 0; child process
   else {
 
-    // this is the child
+    // child process id
     pid = getpid();
 
     printf("I am the child, and my pid is %d\n", pid);
 
-    // Having kill here gets me parent next
+    // Stops while loop in parent process by passing signal.
     kill(getppid(), SIGUSR1);
-    done = 0;
+    sleep(1);
 
     ptr = (char *) shmat(memid, 0, 0);
-    if (ptr == NULL) {
-      printf("shmat() in child failed\n");
-      return(8);
-    }
-
+    // if (ptr == NULL) {
+    //   printf("shmat() in child failed\n");
+    //   return(8);
+    // }
     printf("I am the child, and I read this from the shared memory: '%s'\n", ptr);
 
-    // kill(getppid(), SIGUSR1);
+    // Share memory detach; removes element from shared mem after read by child
     shmdt(ptr);
 
-    ptr = (char *) shmat(memid, 0, 0);
-
     kill(getppid(), SIGUSR1);
-    printf("I am the child, and I read this from the shared memory: '%s'\n", ptr);
+    sleep(1);
 
+    ptr = (char *) shmat(memid, 0, 0);
+    printf("I am the child, and I read this from the shared memory: '%s'\n", ptr);
     shmdt(ptr);
 
+    kill(getppid(), SIGUSR1);
+    sleep(1);
+
     ptr = (char *) shmat(memid, 0, 0);
+    printf("I am the child, and I read this from the shared memory: '%s'\n", ptr);
+    shmdt(ptr);
 
     kill(getppid(), SIGUSR1);
-    printf("I am the child, and I read this from the shared memory: '%s'\n", ptr);
+    sleep(1);
 
+    ptr = (char *) shmat(memid, 0, 0);
+    printf("I am the child, and I read this from the shared memory: '%s'\n", ptr);
     shmdt(ptr);
 
     //kill(getppid(), SIGUSR1);
+    sleep(1);
 
+    //exit(0);
+
+    // Exit both processes once 'done' is read
+    if (ptr == "done") {
+      exit(0);
+    }
   }
 
   shmdt(ptr);
